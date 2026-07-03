9b85c085677e25d0ee95b39d563f92e000c3a2de
ilyar88 <irahmilevich@gmail.com>
2026-07-04 00:11:12 +0300

    Add changes to LoginFlow class.

--- workflows/LoginFlow.js
+++ workflows/LoginFlow.js
@@ -4,14 +4,13 @@ const { verifyEquals } = require('../fixtures/Assert');
 
 // Automates the login page using credentials from environment variables.
 class LoginFlow {
-    // Enters email and password, clicks login, and verifies the page title is 'Client'.
     static async login(page) {
         const loginPage = new Login(page);
         await typeText(loginPage.email(), process.env.EMAIL);
         await typeText(loginPage.password(), process.env.PASSWORD);
         await click(loginPage.login());
         const title = await page.title();
-        verifyEquals(title, 'Client');
+        verifyEquals(title, 'PrintUP test');
         //await checkUI(page, 'Login page UI');
     }
 }
