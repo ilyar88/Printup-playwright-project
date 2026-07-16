8bf43702ed6f97c039bf8bbb79e36d9df4d15dcc
ilyar88 <irahmilevich@gmail.com>
2026-07-16 11:31:18 +0300

    Add changes to the Assert.js file.

--- fixtures/Assert.js
+++ fixtures/Assert.js
@@ -1,25 +1,17 @@
 const { expect } = require('@playwright/test');
 const { allure } = require('allure-playwright');
 
-// Each function wraps an assertion inside an Allure step for reporting
-
 function verifyEquals(actual, expected) {
-    allure.step(`Verify text: ${actual} is equal to ${expected}`, () => {
-        expect(actual, `Verify equals failed: ${actual} is not equal to: ${expected}`).toBe(expected);
-    });
+    expect(actual, `Verify equals failed: ${actual} is not equal to: ${expected}`).toBe(expected);
 }
 
 function verifyText(actual, expected) {
-    allure.step(`Verify text: ${actual} is in ${expected}`, () => {
-        expect(actual, `Verify text failed: '${actual}', does not contain: '${expected}'`).toContain(expected);
-    });
+    expect(actual, `Verify text failed: '${actual}', does not contain: '${expected}'`).toContain(expected);
 }
 
 // Forces the test to fail with a custom message
 function assertFailed(message) {
-    allure.step('Assert failed', () => {
-        expect(false, message).toBeTruthy();
-    });
+    expect(false, message).toBeTruthy();
 }
 
 async function isDisplayed(locator, expected) {
@@ -32,11 +24,9 @@ async function isDisplayed(locator, expected) {
     });
 }
 
-// Soft assert — check if the actual text contains in the expected text, it doesn't fail the test immediately.
+// Soft assert — doesn't fail the test immediately if verification fails
 function verifyTextSoft(actual, expected) {
-    allure.step(`Verify soft assert text: ${actual} is in ${expected}`, () => {
-        expect.soft(actual, `Verify text failed: '${actual}', does not contain: '${expected}'`).toContain(expected);
-    });
+    expect.soft(actual, `Verify text failed: '${actual}', does not contain: '${expected}'`).toContain(expected);
 }
 
 module.exports = { verifyEquals, verifyText, assertFailed, isDisplayed, verifyTextSoft };
