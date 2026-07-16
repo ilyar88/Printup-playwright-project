4b265779d23b5c44e33794779691ae47f918dfb1
ilyar88 <irahmilevich@gmail.com>
2026-07-16 11:56:06 +0300

    Add changes to ListInfoFlow class.

--- workflows/ListInfoFlow.js
+++ workflows/ListInfoFlow.js
@@ -1,9 +1,9 @@
 const ListInfo = require('../pageObjects/ListInfo');
-const { readExcel } = require('../TDD/ExcelReader');
+const { readJson } = require('../fixtures/Hooks');
 const { click } = require('../fixtures/User interface');
 
 class ListInfoFlow {
-    static data = readExcel('ListInfoFlow');
+    static data = readJson('ListInfoFlow');
 
     static async listInfoFlow(page, data) {
         const itemCenter = new ItemCenter(page);
